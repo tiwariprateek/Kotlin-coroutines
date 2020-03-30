d85899435e4b4f90bbe7be509ae9561c3ea684be
Prateek Tiwari <prateektiwari5399@gmail.com>
2020-03-30 20:47:56 +0530

    Implemented Timeout

--- app/src/main/java/com/example/kotlin_coroutines/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_coroutines/MainActivity.kt
@@ -3,29 +3,48 @@ package com.example.kotlin_coroutines
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
-    val RESULT_1="Result #1"
-    val RESULT_2="Result #2"
+    val RESULT_1 = "Result #1"
+    val RESULT_2 = "Result #2"
+    val jobTimeout = 1900L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button.setOnClickListener{
+        button.setOnClickListener {
+            setNewText("Click!!")
             CoroutineScope(IO).launch {
-                fakeApiResult()
+                fakeApiRequest()
             }
         }
     }
-    private fun setNewText(input:String){
-        val text=textView.text.toString() + "\n$input"
+
+    private suspend fun fakeApiRequest() {
+        withContext(IO) {
+            val job = withTimeoutOrNull(jobTimeout) {
+                val result1 = getResult1FromApi()
+                //setNewText(result1)
+                setTextOnMainThread(result1)
+                val result2 = getResult2FromApi()
+                //setNewText(result2)
+                setTextOnMainThread(result2)
+            }
+            if (job == null) {
+                //Toast only works when coroutine are executed on Main thread
+                //Toast.makeText(this@MainActivity,"TimeOut",Toast.LENGTH_LONG).show()
+                Log.d("Debug", "Too long to respond")
+            }
+        }
+    }
+
+    private fun setNewText(input: String) {
+        val text = textView.text.toString() + "\n$input"
         textView.setText(text)
     }
     private suspend fun setTextOnMainThread(input: String){
@@ -33,27 +52,21 @@ class MainActivity : AppCompatActivity() {
             setNewText(input)
         }
     }
-    private suspend fun fakeApiResult(){
-        val result1=getResult1FromApi()
-        setTextOnMainThread(result1)
 
-        val result2=getResult2FromApi()
-        setTextOnMainThread(result2)
-
-
-    }
-    private suspend fun getResult2FromApi():String{
+    private suspend fun getResult2FromApi(): String {
         logThread("getResult2FromApi")
         delay(1000)
         return RESULT_2
     }
-    private suspend fun getResult1FromApi():String{
+
+    private suspend fun getResult1FromApi(): String {
         logThread("getResult1FromApi")
         delay(1000)
         return RESULT_1
     }
 
-    fun logThread(methodName:String){
-        Log.d("Debug","${methodName} is ${Thread.currentThread().name}")
+    fun logThread(methodName: String) {
+        Log.d("Debug", "${methodName} is ${Thread.currentThread().name}")
     }
 }
+
