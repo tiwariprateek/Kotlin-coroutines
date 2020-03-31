44c8154ed2d22cb22c3583b367d9b45b3a288cd7
Prateek Tiwari <prateektiwari5399@gmail.com>
2020-03-31 22:29:28 +0530

    Async and AWAIT

--- app/src/main/java/com/example/kotlin_coroutines/Main3Activity.kt
+++ app/src/main/java/com/example/kotlin_coroutines/Main3Activity.kt
@@ -4,12 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main3.*
-import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import kotlin.system.measureTimeMillis
 
 class Main3Activity : AppCompatActivity() {
@@ -19,14 +16,33 @@ class Main3Activity : AppCompatActivity() {
         setContentView(R.layout.activity_main3)
         button2.setOnClickListener {
             button2.setText("Clicked")
+            fakeApiRequest2()
 
-            fakeApiRequest()
+            //fakeApiRequest1()
         }
     }
-    private fun fakeApiRequest(){
+    private fun fakeApiRequest2(){
+        CoroutineScope(IO).launch {
+            val executionTime= measureTimeMillis {
+                val result1 : Deferred<String> = async {
+                    println("Debug : Launching job1 on ${Thread.currentThread().name}")
+                    getResult1FromApi()
+                }
+                val result2 : Deferred<String> = async {
+                    println("Debug : Launching job1 on ${Thread.currentThread().name}")
+                    getResult2FromApi()
+                }
+                setTextOnMainThread("GOT ${result1.await()}")
+                setTextOnMainThread("GOT ${result2.await()}")
+            }
+            println("Debug : Total time of execution is $executionTime")
+        }
+    }
+    private fun fakeApiRequest1(){
         val startTime=System.currentTimeMillis()
         val parentJob=CoroutineScope(IO).launch {
             val job1= launch {
+                //time1 is stuck inside coroutine
                 val time1 = measureTimeMillis {
                     println("Debug : Launching job1 in ${Thread.currentThread().name} ")
                     val result=getResult1FromApi()
