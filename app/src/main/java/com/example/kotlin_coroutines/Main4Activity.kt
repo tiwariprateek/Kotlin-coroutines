40b410b9453a4c37d5df2011e9f7569bdc648fb7
Prateek Tiwari <prateektiwari5399@gmail.com>
2020-04-01 20:54:28 +0530

    Global scope : The problem child

--- app/src/main/java/com/example/kotlin_coroutines/Main4Activity.kt
+++ app/src/main/java/com/example/kotlin_coroutines/Main4Activity.kt
@@ -3,27 +3,55 @@ package com.example.kotlin_coroutines
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main4.*
-import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.Main
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import kotlin.random.Random
 
 
 class Main4Activity : AppCompatActivity() {
 
     var count=0
+    lateinit var parentjob: Job
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main4)
         //main()
         //executeApirequest()
-        someFunc()
+        //someFunc()
+        anotherFunc()
         button3.setOnClickListener {
-            textView3.setText((count++).toString())
+            //textView3.setText((count++).toString())
+            parentjob.cancel()
         }
     }
+    private suspend fun work(i:Int) {
+        delay(3000)
+        println("Debug : Work $i is done on ${Thread.currentThread().name}")
+    }
+    private fun anotherFunc(){
+        val time=System.currentTimeMillis()
+        println("Debug : Starting parent job.....")
+        parentjob= CoroutineScope(Main).launch {
+            //GlobalScope dosen't sink well with the parent job like
+            //It doesn't stop when the parent job is cancelled
+            //It executes immediately and many more
+            GlobalScope.launch {
+                work(1)
+            }
+            GlobalScope.launch {
+                work(2)
+            }
+        }
+        parentjob.invokeOnCompletion {  throwable ->
+            if (throwable != null){
+                println("Debug : Job was cancelled after ${System.currentTimeMillis()-time}")
+            }
+            else
+                println("Debug : Time for job completion was ${System.currentTimeMillis()-time}")
+        }
+
+    }
+
     private fun someFunc(){
         CoroutineScope(Main).launch {
             println("Debug : The Result1 is ${getresult()}")
